30b08dd59a18c28159a1443a28f0c77d4d333966
Louw Hopley <louw@hopley.co.za>
2018-05-25 00:13:31 +0200

    Basic working version

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# faceblock
-Regain focus by adding/removing Facebook urls to local hosts file to block access.
+# Faceblock - a CLI tool
+
+Regain focus by blocking Facebook access from your local machine. This tool adds or removes Facebook urls to your local hosts file to block access.
+
+## Installation & Usage
+
+Sudo is required to write to your `/etc/hosts` file.
+
+```
+sudo npm install faceblock -g
+```
+
+Blocking access to Facebook:
+```
+faceblock --on
+```
+
+Unblock access to Facebook:
+```
+faceblock --off
+```
+
+## How it works
+
+Faceblock rewrites all hostname lookups for a list of Facebook urls back to your own computer. The following list is added to your `/etc/hosts` file when turning it on:
+
+> 127.0.0.1 www.facebook.com
+127.0.0.1 facebook.com
+127.0.0.1 static.ak.fbcdn.net
+127.0.0.1 www.static.ak.fbcdn.net
+127.0.0.1 login.facebook.com
+127.0.0.1 www.login.facebook.com
+127.0.0.1 fbcdn.net
+127.0.0.1 www.fbcdn.net
+127.0.0.1 fbcdn.com
+127.0.0.1 www.fbcdn.com
+127.0.0.1 static.ak.connect.facebook.com
+127.0.0.1 www.static.ak.connect.facebook.com
---
+++ index.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const hostsFilePath = '/etc/hosts';
+
+const removeBlock = (filePath) => {
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const fileLines = fileContent.split('\n');
+  const startIndex = fileLines.indexOf(`## faceblock-start ##`);
+  const endIndex = fileLines.indexOf(`## faceblock-end ##`);
+  if (startIndex === -1 || endIndex === -1) return;
+  fileLines.splice(startIndex, endIndex - startIndex + 1);
+  const cleanContent = fileLines.join('\n');
+  fs.writeFileSync(filePath, cleanContent);
+};
+
+const addBlock = (filePath) => {
+  const fileContent = fs.readFileSync(filePath, 'utf8');
+  const fileLines = fileContent.split('\n');
+  const blockingLines = [
+    '## faceblock-start ##',
+    '# Block Facebook',
+    '127.0.0.1 www.facebook.com',
+    '127.0.0.1 facebook.com',
+    '127.0.0.1 static.ak.fbcdn.net',
+    '127.0.0.1 www.static.ak.fbcdn.net',
+    '127.0.0.1 login.facebook.com',
+    '127.0.0.1 www.login.facebook.com',
+    '127.0.0.1 fbcdn.net',
+    '127.0.0.1 www.fbcdn.net',
+    '127.0.0.1 fbcdn.com',
+    '127.0.0.1 www.fbcdn.com',
+    '127.0.0.1 static.ak.connect.facebook.com',
+    '127.0.0.1 www.static.ak.connect.facebook.com',
+    '## faceblock-end ##'
+  ];
+  const blockingContent = fileLines.concat(blockingLines).join('\n');
+  fs.writeFileSync(filePath, blockingContent);
+};
+
+// Process command line options
+if (process.argv.includes('--on')) {
+  // Remove the block if it exists
+  removeBlock(hostsFilePath);
+  // Add the blocking lines
+  addBlock(hostsFilePath);
+  console.log(`
+  Facebook has been disabled. Enjoy the focus.
+`);
+} else if (process.argv.includes('--off')) {
+  removeBlock(hostsFilePath);
+  console.log(`
+  Facebook has been enabled again.
+`);
+} else {
+  // All other things should show "help"
+  console.log(`
+  Usage: faceblock [option]
+
+  Options:
+
+    --on    Turn on Facebook blocking
+    --off   Turn of Facebook blocking
+    --help  Show this help message
+`);
+}
